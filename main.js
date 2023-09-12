2ba3d6055b69c2a535e05f55e884a4ab32f8f64d
Andrei Aleksandrovich Shimanskii <72462143+Shamanbanan@users.noreply.github.com>
2023-09-12 12:49:29 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,29 +1,31 @@
-const clientId = "b5fea292ec97459ba91e26e84b146f83";
-const filter = "jpg"; // Фильтр по расширению файлов (например, jpg)
-const imagesContainer = document.getElementById("images");
-
-function getImages() {
-  fetch(`https://cloud-api.yandex.net/v1/disk/resources?`, {
-    headers: {
-      Authorization: `OAuth ${clientId}`,
+window.onload = function () {
+  window.YaAuthSuggest.init(
+    {
+      client_id: "b5fea292ec97459ba91e26e84b146f83",
+      response_type: "token",
+      redirect_uri: "https://examplesite.com/suggest/token"
     },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      const images = data._embedded.items.filter((item) =>
-        item.path.endsWith(`.${filter}`)
-      );
-      imagesContainer.innerHTML = "";
-      images.forEach((image) => {
-        const imageUrl = image.preview;
-        const imageElement = document.createElement("img");
-        imageElement.src = imageUrl;
-        imagesContainer.appendChild(imageElement);
-      });
+    "https://examplesite.com",
+    {
+      view: "button",
+      parentId: "container",
+      buttonView: "main",
+      buttonTheme: "light",
+      buttonSize: "m",
+      buttonBorderRadius: 0
+    }
+  )
+    .then(function (result) {
+      return result.handler();
     })
-    .catch((error) => {
-      console.error("Ошибка получения изображений:", error);
+    .then(function (data) {
+      console.log("Сообщение с токеном: ", data);
+      document.body.innerHTML += `Сообщение с токеном: ${JSON.stringify(data)}`;
+    })
+    .catch(function (error) {
+      console.log("Что-то пошло не так: ", error);
+      document.body.innerHTML += `Что-то пошло не так: ${JSON.stringify(
+        error
+      )}`;
     });
-}
-
-getImages();
+};
